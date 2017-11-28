0f3d30aac6b8dc940608a2fca787fb92bf8d23ba
Kel Kanhirun <kel.kanhirun@gmail.com>
2017-11-28 09:47:03 -0600

    Adds basic maps functionality

--- YetAnotherSocialApp/AppDelegate.swift
+++ YetAnotherSocialApp/AppDelegate.swift
@@ -7,15 +7,18 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        // Starts Google Maps
+        GMSServices.provideAPIKey(Bundle.main.object(forInfoDictionaryKey: "GOOGLE_API_KEY") as! String)
+
         return true
     }
 
--- YetAnotherSocialApp/ViewController.swift
+++ YetAnotherSocialApp/ViewController.swift
@@ -1,14 +1,17 @@
-//
-//  ViewController.swift
-//  YetAnotherSocialApp
-//
-//  Created by Feynman on 11/27/17.
-//  Copyright © 2017 Kevin Kanhirun. All rights reserved.
-//
-
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
+
+    override func loadView() {
+        super.loadView()
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
